c42dfbd12159e571794574d89ab2b13a24b6ff92
Johan Commelin <johan@commelin.net>
2020-07-15 20:10:39 +0000

    ramanujan

--- src/solutions/thursday/morning/groups_rings_fields.lean
+++ src/solutions/thursday/morning/groups_rings_fields.lean
@@ -1,11 +1,12 @@
 import linear_algebra.finite_dimensional
 import ring_theory.algebraic
 import data.zmod.basic
+import data.real.basic
 import tactic
 
 
-section exercise1
-/-
+section
+/-! ## Exercise 1
 We will warm up with a well-known result:
 “Subgroups of abelian groups are normal.”
 
@@ -29,10 +30,12 @@ end
 
 end add_subgroup
 
-end exercise1
+end
 
-section exercise2
-/-
+
+
+section
+/-! ## Exercise 2
 The following exercise will not be completely straight-forward.
 We will prove a result that is not yet in mathlib:
 “Finite field extensions are algebraic.”
@@ -57,10 +60,13 @@ end
 
 end algebra
 
-end exercise2
+end
 
-section exercise3
-/-
+
+
+
+section
+/-! ## Exercise 3
 The next exercise asks to show that a monic polynomial `f ∈ ℤ[X]` is irreducible
 if it is irreducible modulo a prime `p`.
 This fact is also not in mathlib.
@@ -122,9 +128,82 @@ end
 
 end polynomial
 
-end exercise3
+end
 
-example : true :=
+
+
+
+section
+/-! ## Bonus exercise (wicked hard) -/
+
+noncomputable theory       -- because `polynomial` is noncomputable (implementation detail)
+open_locale big_operators  -- get access to nice notation: ∑ and ∏
+open polynomial            -- we want to write `X`, instead of `polynomial.X`
+
+/-
+First we make some definitions
+Scroll to the end for the actual exercise
+-/
+
+def partial_ramanujan_tau_polynomial (n : ℕ) : polynomial ℤ :=
+X * ∏ k in finset.Ico 1 n, (1 - X^k)^24
+
+def ramanujan_tau (n : ℕ) : ℤ :=
+coeff (partial_ramanujan_tau_polynomial n) n
+
+-- Some nice suggestive notation
+prefix `τ`:300 := ramanujan_tau
+
+/-
+Some lemmas to warm up
+-/
+
+lemma ramanujan_tau_zero : τ 0 = 0 :=
 begin
-  exact x
+  -- sorry
+  simp [ramanujan_tau, partial_ramanujan_tau_polynomial],
+  -- sorry
+end
+
+lemma ramanujan_tau_one : τ 1 = 1 :=
+begin
+  -- sorry
+  simp [ramanujan_tau, partial_ramanujan_tau_polynomial],
+  -- sorry
+end
+
+-- This one is nontrivial
+
+lemma ramanujan_tau_two : τ 2 = -24 :=
+begin
+  -- sorry
+  -- I did not finish this proof
+  -- I also did not clean up what I wrote
+  -- Really, we ought to have a tactic that makes this easy
+  delta ramanujan_tau partial_ramanujan_tau_polynomial,
+  rw [mul_comm, coeff_mul_X],
+  suffices : ((1 - X) ^ 24 : polynomial ℤ).coeff 1 = -24, by simpa,
+  rw [sub_eq_add_neg, add_comm, add_pow],
+  rw [finset_sum_coeff, finset.sum_eq_single 1],
+  { simp, rw [mul_comm, coeff_mul_X],
+    sorry },
+  { simp, intros,
+    rw [neg_pow, mul_assoc, mul_comm (X ^ b), ← mul_assoc],
+    sorry },
+  { rw finset.mem_range, exact dec_trivial }
+  -- sorry
+end
+
+/-
+The actual exercise. Good luck (-;
+-/
+
+theorem deligne (p : ℕ) (hp : p.prime) : (abs (τ p) : ℝ) ≤ 2 * p^(11/2) :=
+begin
+  -- sorry
+  -- I did not even start this proof
+  sorry
+  -- sorry
+end
+
 end
