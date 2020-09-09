3f428cf46c35d410251ea76986d0d3ee08c2c091
Scott Morrison <scott.morrison@gmail.com>
2020-09-10 09:08:22 +1000

    fix exercise

--- src/exercises_sources/thursday/category_theory/exercise7.lean
+++ src/exercises_sources/thursday/category_theory/exercise7.lean
@@ -9,43 +9,43 @@ open category_theory
 
 variables (C : Type*) [category C] [monoidal_category C]
 
-structure Mon_in :=
+structure Mon_ :=
 (X : C)
 (ι : 𝟙_ C ⟶ X)
 (μ : X ⊗ X ⟶ X)
 -- There are three missing axioms here!
 -- Use `λ_ X`, `ρ_ X` and `α_ X Y Z` for unitors and associators.
-sorry
 
-namespace Mon_in
+namespace Mon_
 
 variables {C}
 
 @[ext]
-structure hom (M N : Mon_in C) :=
+structure hom (M N : Mon_ C) :=
+(hom : M.X ⟶ N.X)
+-- What are the axioms?
+
+instance : category (Mon_ C) :=
 sorry
 
-
-
-instance : category (Mon_in C) :=
-sorry
-
-end Mon_in
+end Mon_
 
 /-!
+(Note: since LFTCM2020, this goal of this exercise has been PR'd to mathlib,
+under `category_theory.monoidal.internal`, along with several of the projects listed below.)
+
 Bonus projects (all but the first will be non-trivial with today's mathlib):
-* Construct the category of module objects for a fixed monoid object.
-* Check that `Mon_in Type ≌ Mon`.
-* Check that `Mon_in Mon ≌ CommMon`, via the Eckmann-Hilton argument.
+* ✓ Construct the category of module objects for a fixed monoid object.
+* ✓ Check that `Mon_ Type ≌ Mon`.
+* Check that `Mon_ Mon ≌ CommMon`, via the Eckmann-Hilton argument.
   (You'll have to hook up the cartesian monoidal structure on `Mon` first.)
-* Check that `Mon_in AddCommGroup ≌ Ring`.
+* Check that `Mon_ AddCommGroup ≌ Ring`.
   (You'll have to hook up the monoidal structure on `AddCommGroup`.
   Currently we have the monoidal structure on `Module R`; perhaps one could specialize to `R = ℤ`
   and transport the monoidal structure across an equivalence? This sounds like some work!)
-* Check that `Mon_in (Module R) ≌ Algebra R`.
+* ✓ Check that `Mon_ (Module R) ≌ Algebra R`.
 * Show that if `C` is braided (you'll have to define that first!)
-   then `Mon_in C` is naturally monoidal.
+   then `Mon_ C` is naturally monoidal.
 * Can you transport this monoidal structure to `Ring` or `Algebra R`?
   How does it compare to the "native" one?
 -/
-
