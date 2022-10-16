ce3b00b992db07aa9bc1d2a7cb567e78f031f685
Scott Morrison <scott.morrison@gmail.com>
2022-10-16 17:27:49 +1100

    remove obsolete attribute

--- src/solutions/thursday/category_theory/exercise2.lean
+++ src/solutions/thursday/category_theory/exercise2.lean
@@ -7,10 +7,6 @@ Let's show that taking polynomials over a ring is functor `Ring ⥤ Ring`.
 
 noncomputable theory -- the default implementation of polynomials is noncomputable
 
--- Just ignore this for now: it's a hack that prevents an annoying problem,
--- and a cleaner fix is on its way to mathlib.
-local attribute [irreducible] polynomial.eval₂
-
 /-!
 Hints:
 * use `polynomial.map_ring_hom`
@@ -47,55 +43,3 @@ def commutes :
 There are some further hints in
 `hints/category_theory/exercise2/`
 -/
-
-/-!
-Bonus problem:
-
-Why did we set `local attribute [irreducible] polynomial.eval₂`?
-What goes wrong without it? Why?
--/
--- omit
-
-local attribute [semireducible] polynomial.eval₂
-
-/- def Ring.polynomial' : Ring ⥤ Ring :=
-{ obj := λ R, Ring.of (polynomial R),
-  map := λ R S f, ring_hom.of (polynomial.map f),
-  map_comp' := λ R S T f g, begin refl end, }.  -/
-
-  -- fails, but takes >5s seconds to do so!
-
-/-!
-What's going on?
-
-For some reason trying to solve the goal in `map_comp'` using `refl` takes a huge amount of time.
-This causes the automation which is responsible for
-automatically proving functoriality of functors to time out, and fail.
-
-How can `refl` take so long? Isn't it just checking if two things are the same?
-The problem is that when we work in type theory, in principle no definition is "opaque",
-and Lean sometimes has to unfold definitions in order to compare if two things are actually the same.
-
-Usually it's pretty conservative about this, and manages to avoid unfolding too deeply
-before coming up with an answer, but in bad cases it can get really bad.
-
-Somehow this is happening here!
-
-The solution we have available is to mark definitions as `[irreducible]`,
-which (almost, but not quite completely) prevents Lean from unfolding when checking
-definitional equality. Of course, this has a consequence --- Lean will sometimes
-fail to prove things by `refl` now!
-
-The solution to this is to provide a thorough API for our important definitions,
-so that after some point in the development one never needs to unfold the actual definition again,
-but one can work through theorems proved about the definition
-(e.g. characterisations and universal properties).
-
-An important example of this is that we have marked `real` as `[irreducible]` in Lean ---
-after you've got things working, no one should ever have to know whether the "actual definition"
-is in terms of Cauchy sequences or Dedekind cuts.
-
-Unfortunately at this point we don't use `[irreducible]` often enough in Lean, and improving this
-aspect of mathlib is an ongoin project.
--/
--- omit
